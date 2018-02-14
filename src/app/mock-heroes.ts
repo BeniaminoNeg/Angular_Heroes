04e9e6c3adc61f42466cfd1244d1dfac860cfd4c
Beniamino Negrini <beniamino.negrini@nttdata.com>
2018-02-14 16:09:43 +0100

    Seconda sez

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import {Hero} from './hero';
+
+// Array di eroi
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Cialente' },
+  { id: 12, name: 'Biondi' },
+  { id: 13, name: 'Schulz' },
+  { id: 14, name: 'Black' },
+  { id: 15, name: 'Picuriju' },
+  { id: 16, name: 'Gisù' },
+  { id: 17, name: 'Settecafè' },
+  { id: 18, name: 'Mucchittu' },
+  { id: 19, name: 'Bellò' },
+  { id: 20, name: 'Sapò' }
+];
